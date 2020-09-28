7232c337ca5d8069e246a07d9e036b6366ffff93
AriAlavi <arianalavi1@gmail.com>
2020-09-28 16:07:35 -0700

    No longer supported

--- run_gpu_signn.sh
+++
@@ -1,1 +0,0 @@
-sudo GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/multi_hand_tracking/multi_hand_tracking_gpu --calculator_graph_config_file=mediapipe/graphs/hand_tracking/multi_hand_tracking_desktop_signn.pbtxt
