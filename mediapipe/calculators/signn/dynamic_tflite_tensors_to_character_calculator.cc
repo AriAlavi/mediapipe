7ae2f925b969a752e3705730b6aaeaa031c6414f
AriAlavi <arianalavi1@gmail.com>
2020-09-28 17:25:54 -0700

    Fixed not working on Android by hard-coding

--- mediapipe/calculators/signn/dynamic_tflite_tensors_to_character_calculator.cc
+++ mediapipe/calculators/signn/dynamic_tflite_tensors_to_character_calculator.cc
@@ -2,9 +2,7 @@
 #include "tensorflow/lite/interpreter.h"
 #include "mediapipe/calculators/signn/dynamic_tflite_tensors_to_character_calculator.pb.h"
 #include "mediapipe/calculators/signn/timed_queue.h"
-
 #include <vector>
-
 namespace mediapipe {
     constexpr char STRING[] = "STRING";
 
@@ -18,12 +16,10 @@ namespace mediapipe {
   private:
     float unknown_threshold = 0;
     float memory_length = 2;
-    TimedQueue<bool> J_Probs;
-    TimedQueue<bool> Z_Probs;
-    std::string J = "J";
-    std::string Z = "Z";
-    std::vector<std::string> DATA_MAP;
-    std::vector<TimedQueue<bool>> probs = {J_Probs, Z_Probs};
+    float last_character_bias = .001;
+    int last_character = -1;
+    TimedQueue<bool> J_probs;
+    TimedQueue<bool> Z_probs;
   };
 
   REGISTER_CALCULATOR(DynamicTfLiteTensorsToCharacterCalculator);
@@ -37,103 +33,59 @@ namespace mediapipe {
 
   ::mediapipe::Status DynamicTfLiteTensorsToCharacterCalculator::Open(
       CalculatorContext* cc) {
-    DATA_MAP.push_back(J);
-    DATA_MAP.push_back(Z);
     const auto& options = cc->Options<::mediapipe::DynamicTfliteTensorsToCharacterCalculatorOptions>();
+    // unknown_threshold = options.unknown_threshold();
+    // last_character_bias = options.memo();
     unknown_threshold = options.unknown_threshold();
     memory_length = options.memory_length();
-    for(int i = 0; i < probs.size(); i++){
-      probs.at(i) = TimedQueue<bool>(memory_length);
-      // probs.at(i).add(true);
-    }
+    J_probs = TimedQueue<bool>(memory_length);
+    Z_probs = TimedQueue<bool>(memory_length);
     return ::mediapipe::OkStatus();
   }
 
   ::mediapipe::Status DynamicTfLiteTensorsToCharacterCalculator::Process(
       CalculatorContext* cc) {
     const auto& input_tensors = cc->Inputs().Tag("TENSORS").Get<std::vector<TfLiteTensor>>();
-      const TfLiteTensor* raw_tensor = &input_tensors[0];
-
-
-      const float* raw_floats = raw_tensor->data.f;
-      int num_values = 1;
-      for (int i = 0; i < raw_tensor->dims->size; ++i) {
-        RET_CHECK_GT(raw_tensor->dims->data[i], 0);
-        num_values *= raw_tensor->dims->data[i];
-      }
-      auto output_floats = absl::make_unique<std::vector<float>>(
-          raw_floats, raw_floats + num_values);
-      
-      float highest_confidence = 0;
-      int highest_confidence_index = 0;
-      for(int i = 0; i < output_floats->size(); i++){
-        if(output_floats->at(i) > highest_confidence_index){
-          highest_confidence = output_floats->at(i);
-          highest_confidence_index = i;
-        }
-      }
-      probs.at(highest_confidence_index).add(true);
-
-      int most_common_index = 0;
-      int most_common_count = 0;
-      int total_count = 0;
-      for(int i = 0; i < DATA_MAP.size(); i++){
-        int current_size = probs.at(i).get().size();
-        if(current_size > most_common_count){
-          most_common_count = current_size;
-          total_count += current_size;
-          most_common_index = i;
-        }
-      }
-      float certainty = (float)most_common_count / (float)total_count;
-      std::string signn;
-      if(certainty >= unknown_threshold){
-        signn = DATA_MAP[most_common_index];
-      }else{
-        signn = "Unknown";
-      }
-
-    LOG(INFO) << DATA_MAP[most_common_index] << ": " << certainty << "% count: " << most_common_count;
-
-
-
-    // float highest_confidence = 0;
-    // int highest_confidence_index = -1;
-    // if(output_floats->size() != DATA_MAP.size()){
-    //   LOG(INFO) << output_floats->size() << " OUTPUT FLOATS BUT " << DATA_MAP.size() << " IN DATA MAP";
-    // }
-    // for(int i = 0; i < output_floats->size(); i++){
-    //   if(output_floats->at(i) > highest_confidence){
-    //     highest_confidence = output_floats->at(i);
-    //     highest_confidence_index = i;
-    //   }
-    // }
-    // probs.at(highest_confidence_index).add(true);
+    const TfLiteTensor* raw_tensor = &input_tensors[0];
     
-    // int most_frames = 0;
-    // int most_frames_index = 0;
-    // int total_frames = 0;
 
+    const float* raw_floats = raw_tensor->data.f;
+    int num_values = 1;
+    for (int i = 0; i < raw_tensor->dims->size; ++i) {
+      RET_CHECK_GT(raw_tensor->dims->data[i], 0);
+      num_values *= raw_tensor->dims->data[i];
+    }
 
-    // for(int i = 0; i < probs.size(); i++){
-    //   if(probs.at(i).get().size() > most_frames){
-    //     most_frames = probs.at(i).get().size();
-    //     most_frames_index = i;
-    //   }
-    //   total_frames += probs.at(i).get().size();
-    // }
-    // if(total_frames == 0){
-    //   total_frames = 1;
-    // }
+    auto output_floats = absl::make_unique<std::vector<float>>(
+        raw_floats, raw_floats + num_values);
+    
+    if(output_floats->at(0) > output_floats->at(1)){
+      J_probs.add(true);
+    }else{
+      Z_probs.add(true);
+    }
 
-
-    // std::string signn;
-    // if(most_frames / total_frames >= unknown_threshold){
-    //   signn = DATA_MAP[most_frames_index];
-    // }else{
-    //   signn = "Unknown";
-    // }
-    // LOG(INFO) << signn << ", " <<  most_frames / total_frames;
+    int J_size = J_probs.get().size();
+    int Z_size = Z_probs.get().size();
+    int higher_size = 0;
+    int total_size = J_size + Z_size;
+    std::string signn;
+    if(total_size == 0){
+      total_size == 1;
+    }
+    float confidence = 0;
+    if(J_size > Z_size){
+      signn = "J";
+      higher_size = J_size;
+    }else{
+      signn = "Z";
+      higher_size = Z_size;
+    }
+    confidence = (float)higher_size / (float)total_size;
+    if(confidence < unknown_threshold){
+      signn = "Unknown";
+    }
+    LOG(INFO) << signn << ": " << confidence << "%";
 
     std::unique_ptr<std::string> output_stream_collection = std::make_unique<std::string>(signn); 
     cc -> Outputs().Tag(STRING).Add(output_stream_collection.release(), cc->InputTimestamp());
